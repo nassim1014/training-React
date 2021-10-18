7619f05a674c2ca4276410719e02a28f9c2f4421
nassim1014 <heelnassim@gmail.com>
2021-10-18 21:51:32 +0100

    Pedro tech video 7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import Hello from './Componets/Hello.js'
+import React ,{useState}from 'react'
 import './App.css';
-
 function App() {
+  var username='Nassim'
+  var password=123
+  const [name,setname]= useState('')
+  const [passwordstat,setpassword]= useState(password)
+  const [logedin,setlogin]= useState(false)
+  function login(){
+    if(username == name && passwordstat== password ){
+      setlogin(true)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1>Login</h1>
+    <input type={Text} onChange = {
+      (event)=> {setname(event.target.value);}
+      }>
+
+    </input>
+    <input type={'password'}
+    onChange={(event)=>{
+      setpassword(event.target.value)
+    }}></input>
+    <button onClick={login}>Submit</button>
     </div>
+
   );
 }
 
---
+++ src/Componets/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Hello({myname}) {
+    return (
+        <div>
+            <button>change age</button>
+            <input type="text" />
+            <h1>{myname}</h1>
+            <hr />
+        </div>
+    )
+}
